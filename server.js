1606c67e037317f2ad0d93453833117ce6239c7d
Mehmetwasd <mehmet@example.com>
2026-04-08 10:06:10 +0300

    fix render deploy

--- server.js
+++ server.js
@@ -7,15 +7,12 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/api/test', (req, res) => {
-  res.json({ message: "Server is running 🚀" });
+  res.json({ message: 'Server is running 🚀' });
 });
 
 app.post('/api/start', (req, res) => {
-  console.log("Project started 🔥");
-
-  res.json({
-    message: "Project started 🚀"
-  });
+  console.log('Project started 🔥');
+  res.json({ message: 'Project started 🚀' });
 });
 
 app.listen(PORT, () => {
